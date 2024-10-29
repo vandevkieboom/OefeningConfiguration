dadee41f8dbd32e6683c22c6eeacc4caabfb10fb
Jordy Van Den Kieboom <vandevkieboom@gmail.com>
2024-10-29 21:06:52 +0100

    Add project files.

---
+++ OefeningConfiguration/Controllers/ConfigurationController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OefeningConfiguration.Models;
+
+namespace OefeningConfiguration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigurationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigurationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("direct-from-appsettings")]
+        public ActionResult GetApplicationSettings()
+        {
+            var appName = _configuration["ApplicationSettings:AppName"];
+            var version = _configuration["ApplicationSettings:Version"];
+            var maxUsers = _configuration["ApplicationSettings:MaxUsers"];
+
+            return Ok(new { AppName = appName, Version = version, MaxUsers = maxUsers});
+        }
+
+        [HttpGet]
+        [Route("using-binder")]
+        public ActionResult GetApplicationSettingsWithBind()
+        {
+            var applicationSettings = new ApplicationSettings();
+            _configuration.GetSection(ApplicationSettings.SectionName).Bind(applicationSettings);
+
+            return Ok(new { applicationSettings.AppName, applicationSettings.Version, applicationSettings.MaxUsers });
+        }
+    }
+}
---
+++ OefeningConfiguration/Models/ApplicationSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace OefeningConfiguration.Models
+{
+    public class ApplicationSettings
+    {
+        public const string SectionName = "ApplicationSettings";
+        public string AppName { get; set; } = string.Empty;
+        public string Version { get; set; } = string.Empty;
+        public int MaxUsers { get; set; }
+    }
+}
